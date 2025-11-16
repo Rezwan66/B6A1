7c32607c56679aa8de1d56139f1ca6aa6078a0d7
Rezwan66 <shaikhrezwan66@gmail.com>
2025-11-16 21:42:31 +0100

    problem 1 solve

--- solution.ts
+++ solution.ts
@@ -1,1 +1,12 @@
-// solutions
+function formatValue(
+  value: string | number | boolean
+): string | number | boolean | undefined {
+  if (typeof value === 'string') {
+    return value.toUpperCase();
+  } else if (typeof value === 'number') {
+    return value * 10;
+  } else if (typeof value === 'boolean') {
+    return !value;
+  }
+  return undefined;
+}
